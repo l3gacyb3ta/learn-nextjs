8a92491ff391f71b67fd0c049cf6231debec7769
Gitea <gitea@fake.local>
2021-04-25 13:30:53 -0400

    Commited auto-magically because l3gacy was too lazy to actually write a commit message for this.

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,21 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+
+
 export default function FirstPost() {
-    return <h1>First Post</h1>
-}  
+  return (
+    <>
+      <Head>
+          <title>First Post</title>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"></link>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
