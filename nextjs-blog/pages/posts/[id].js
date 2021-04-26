d5217b80e03b6d211bebf70e82a2270ec84a6bc2
Gitea <gitea@fake.local>
2021-04-25 20:51:20 -0400

    Commited auto-magically because l3gacy was too lazy to actually write a commit message for this.

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -2,7 +2,7 @@ import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -22,11 +22,15 @@ export async function getStaticPaths({ params }) {
 export default function Post({ postData }) {
     return (
       <Layout>
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
         {postData.title}
         <br />
         {postData.id}
         <br />
         {postData.date}
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
     )
 }  
