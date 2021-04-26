2be0c0a08d656c81630bcb5a508a5b095e0a70d8
Gitea <gitea@fake.local>
2021-04-25 20:44:21 -0400

    Commited auto-magically because l3gacy was too lazy to actually write a commit message for this.

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Markdown from 'markdown-to-jsx';
+// import Markdown from 'markdown-to-jsx';
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
@@ -21,13 +21,20 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-
-        <p><Markdown>{intro}</Markdown></p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
